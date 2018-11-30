43672aebdb75461d55520df7fbcd667f49b30773
joldie <j.oldridge@mykolab.com>
2018-11-30 16:25:23 +0100

    Add wiki-vital-articles npm package, remove local dependency.

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const vitalArticles = require("../modules/wikipedia-vital-articles.js");
+const wikiVitalArticles = require("wiki-vital-articles");
 
 // Enable CORS for all origins
 app.use(cors());
 
 app.get("/", async function(req, res) {
   // Get list of articles by scraping Wikipedia webpage
-  const articles = await vitalArticles();
+  const articles = await wikiVitalArticles();
   // Set headers and send JSON object containing all articles
   res.setHeader("Content-Type", "application/json");
   res.send(articles);
